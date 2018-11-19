08fd97940b5333021773ddcd09693fb103513f8d
차고음 <phto_kuro@naver.com>
2018-11-19 14:28:02 -0500

    Initial Commit

---
+++ ActiveSL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActiveSL
+//
+//  Created by GoEum Cha on 11/19/18.
+//  Copyright © 2018 GoEum Cha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ActiveSLTests/ActiveSLTests.swift
@@ -0,0 +1,34 @@
+//
+//  ActiveSLTests.swift
+//  ActiveSLTests
+//
+//  Created by GoEum Cha on 11/19/18.
+//  Copyright © 2018 GoEum Cha. All rights reserved.
+//
+
+import XCTest
+@testable import ActiveSL
+
+class ActiveSLTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
